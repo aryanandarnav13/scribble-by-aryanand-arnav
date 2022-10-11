724d90d48832abfc9eaf96cdb79f5400feac2e2f
Aryanand Arnav <56192760+aryanandarnav13@users.noreply.github.com>
2022-10-12 04:06:12 +0530

    Added MenuBar and general Settings UI

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -7,6 +7,7 @@ import { initializeLogger } from "common/logger";
 import Dashboard from "components/Dashboard";
 import NewArticle from "components/Dashboard/Articles/NewArticle/Create";
 import EditArticle from "components/Dashboard/Articles/NewArticle/Edit";
+import Settings from "components/Dashboard/Settings";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -26,6 +27,7 @@ const App = () => {
         <Route exact component={Dashboard} path="/" />
         <Route exact component={NewArticle} path="/articles/create" />
         <Route exact component={EditArticle} path="/articles/:slug/edit" />
+        <Route exact component={Settings} path="/settings" />
       </Switch>
     </Router>
   );
