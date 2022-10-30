7d8eecfe5fdcbd1c9d7669af56e6312b81d09569
Aryanand Arnav <56192760+aryanandarnav13@users.noreply.github.com>
2022-10-31 00:30:45 +0530

    Fixed Tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -7,15 +7,15 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     @website = create(:website)
   end
 
-  def test_should_login_user_with_valid_credentials
-    post session_path, params: { login: { password: @website.password } }, as: :json
-    assert_response :success
-    assert_equal response.parsed_body["authentication_token"], @website.authentication_token
-  end
-
   def test_shouldnt_login_user_with_invalid_credentials
     post session_path, params: { login: { password: "invalid password" } }, as: :json
     assert_response :unauthorized
-    assert_equal response.parsed_body["error"], "incorrect_credentials"
+    assert_equal response.parsed_body["error"], "Incorrect credentials"
+  end
+
+  def test_should_login_user_with_valid_credentials
+    post session_path, params: { password: @website.password }, as: :json
+    assert_response :success
+    assert_equal response.parsed_body["authentication_token"], @website.authentication_token
   end
 end
