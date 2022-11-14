6b696ffee0d099924d8abde931ac6f6c6526b48d
Aryanand Arnav <56192760+aryanandarnav13@users.noreply.github.com>
2022-11-14 18:49:59 +0530

    Fix deployment issue (#113)

--- db/migrate/20221026225617_add_user_references_to_article.rb
+++ db/migrate/20221026225617_add_user_references_to_article.rb
@@ -2,6 +2,7 @@
 
 class AddUserReferencesToArticle < ActiveRecord::Migration[6.1]
   def change
+    Article.reset_column_information
     add_column :articles, :user_id, :integer
     add_foreign_key :articles, :users, on_delete: :cascade
   end
