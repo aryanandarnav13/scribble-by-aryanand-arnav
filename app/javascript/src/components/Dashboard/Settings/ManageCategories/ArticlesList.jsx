7b6f394685cc35b8ccc65aee3e360e37f2dd7f12
Aryanand Arnav <56192760+aryanandarnav13@users.noreply.github.com>
2022-11-22 22:09:17 +0530

    Fixed Srollable issue

--- app/javascript/src/components/Dashboard/Settings/ManageCategories/ArticlesList.jsx
+++ app/javascript/src/components/Dashboard/Settings/ManageCategories/ArticlesList.jsx
@@ -67,85 +67,93 @@ const ArticlesList = ({
   };
 
   return (
-    <DragDropContext onDragEnd={handleOnDragEnd} onDragStart={handleDragStart}>
-      <Droppable droppableId="articlesList">
-        {provided => (
-          <Scrollable {...provided.droppableProps} ref={provided.innerRef}>
-            {articles.map((article, index) => (
-              <Draggable
-                draggableId={article.id.toString()}
-                index={index}
-                key={article.id}
-              >
-                {provided => (
-                  <div
-                    ref={provided.innerRef}
-                    {...provided.dragHandleProps}
-                    {...provided.draggableProps}
-                    className={
-                      selectedArticle === article.id
-                        ? "bg-gray-50"
-                        : "white border-t"
-                    }
-                  >
-                    <div className="neeto-ui-shadow-xs neeto-ui-rounded-none neeto-ui-border-gray-400 border mb-4 w-full space-y-2 p-5">
-                      <div>
-                        <Checkbox
-                          id="checkbox_name_disabled"
-                          onClick={() => handleArticles(article.id)}
-                        />
-                        <Typography className="mt-2" style="h4">
-                          {article.title}
-                        </Typography>
-                      </div>
-                      <div className="mb-2">
-                        <Typography className="truncate text-sm" style="body2">
-                          {article.body}
-                        </Typography>
-                      </div>
-                      <hr />
-                      <div className="mt-3 flex align-middle ">
-                        <div className="item-center ml-auto flex space-x-2">
-                          <Clock className="mt-2" size="20" />
-                          <Tooltip
-                            content={weekDaydateFormat(article.created_at)}
-                            position="bottom"
-                          >
-                            <Typography className="mt-2" style="body3">
-                              {`${
-                                article.status === "Publish"
-                                  ? "Published"
-                                  : "Drafted"
-                              } ${calculateCreatedAgo(article.updated_at)} `}
-                            </Typography>
-                          </Tooltip>
+    <Scrollable>
+      <DragDropContext
+        onDragEnd={handleOnDragEnd}
+        onDragStart={handleDragStart}
+      >
+        <Droppable droppableId="articlesList">
+          {provided => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {articles.map((article, index) => (
+                <Draggable
+                  draggableId={article.id.toString()}
+                  index={index}
+                  key={article.id}
+                >
+                  {provided => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.dragHandleProps}
+                      {...provided.draggableProps}
+                      className={
+                        selectedArticle === article.id
+                          ? "bg-gray-50"
+                          : "white border-t"
+                      }
+                    >
+                      <div className="neeto-ui-shadow-xs neeto-ui-rounded-none neeto-ui-border-gray-400 border mb-4 w-full space-y-2 p-5">
+                        <div>
+                          <Checkbox
+                            id="checkbox_name_disabled"
+                            onClick={() => handleArticles(article.id)}
+                          />
+                          <Typography className="mt-2" style="h4">
+                            {article.title}
+                          </Typography>
                         </div>
-                        <Avatar
-                          className="ml-2 mt-1"
-                          size="small"
-                          user={{
-                            name: article.author,
-                          }}
-                        />
-                        <Tag
-                          className="neeto-ui-rounded-none ml-2 bg-gray-200"
-                          label={article.status}
-                          type="solid"
-                          style={
-                            article.status === "Publish" ? "info" : "warning"
-                          }
-                        />
+                        <div className="mb-2">
+                          <Typography
+                            className="truncate text-sm"
+                            style="body2"
+                          >
+                            {article.body}
+                          </Typography>
+                        </div>
+                        <hr />
+                        <div className="mt-3 flex align-middle ">
+                          <div className="item-center ml-auto flex space-x-2">
+                            <Clock className="mt-2" size="20" />
+                            <Tooltip
+                              content={weekDaydateFormat(article.created_at)}
+                              position="bottom"
+                            >
+                              <Typography className="mt-2" style="body3">
+                                {`${
+                                  article.status === "Publish"
+                                    ? "Published"
+                                    : "Drafted"
+                                } ${calculateCreatedAgo(article.updated_at)} `}
+                              </Typography>
+                            </Tooltip>
+                          </div>
+                          <Avatar
+                            className="ml-2 mt-1"
+                            size="small"
+                            user={{
+                              name: article.author,
+                            }}
+                          />
+                          <Tag
+                            className="neeto-ui-rounded-none ml-2 bg-gray-200"
+                            label={article.status}
+                            type="solid"
+                            style={
+                              article.status === "Publish" ? "info" : "warning"
+                            }
+                          />
+                        </div>
                       </div>
                     </div>
-                  </div>
-                )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
-          </Scrollable>
-        )}
-      </Droppable>
-    </DragDropContext>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </Scrollable>
   );
 };
 export default ArticlesList;
