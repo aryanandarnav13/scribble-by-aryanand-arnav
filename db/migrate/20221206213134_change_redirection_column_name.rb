5bf0d063caa82dc9aed0a1434d2704d012518859
Aryanand Arnav <56192760+aryanandarnav13@users.noreply.github.com>
2022-12-07 16:39:59 +0530

    Removed unneccessary migration

--- db/migrate/20221206213134_change_redirection_column_name.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class ChangeRedirectionColumnName < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :redirections, :frompath, :from
-    rename_column :redirections, :topath, :to
-  end
-end
