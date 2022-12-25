bc0658166ac1cfee67a7febd17c94e4f4b6bfbe0
Aryanand Arnav <56192760+aryanandarnav13@users.noreply.github.com>
2022-12-26 00:26:28 +0530

    Added zustand to fetch categories at the dashboard

---
+++ app/javascript/src/store.js
@@ -0,0 +1,8 @@
+import create from "zustand";
+
+const useCategoryStore = create(set => ({
+  categories: [],
+  setCategories: categories => set({ categories }),
+}));
+
+export default useCategoryStore;
